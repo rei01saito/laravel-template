ffe00a4cf94bb17f886e79d045e5e07eb72e6a22
rei01saito <rei01saito@gmail.com>
2023-07-15 12:33:22 +0900

    minioの設定修正

--- config/filesystems.php
+++ config/filesystems.php
@@ -46,14 +46,14 @@ return [
 
         's3' => [
             'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID', env('MINIO_ACCESS_KEY_ID')),
-            'secret' => env('AWS_SECRET_ACCESS_KEY', env('MINIO_SECRET_ACCESS_KEY')),
-            'region' => env('AWS_DEFAULT_REGION', env('MINIO_DEFAULT_REGION')),
-            'bucket' => env('AWS_BUCKET', env('MINIO_BUCKET')),
-            'url' => env('AWS_URL', env('MINIO_URL')),
-            'endpoint' => env('AWS_ENDPOINT', env('MINIO_ENDPOINT')),
+            'key' => env('MINIO_ACCESS_KEY_ID', env('AWS_ACCESS_KEY_ID')),
+            'secret' => env('MINIO_SECRET_ACCESS_KEY', env('AWS_SECRET_ACCESS_KEY')),
+            'region' => env('MINIO_DEFAULT_REGION', env('AWS_DEFAULT_REGION')),
+            'bucket' => env('MINIO_BUCKET', env('AWS_BUCKET')),
+            'url' => env('MINIO_URL', env('AWS_URL')),
+            'endpoint' => env('MINIO_ENDPOINT', env('AWS_ENDPOINT')),
             'visibility' => 'private',
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', env('MINIO_USE_PATH_STYLE_ENDPOINT', false)),
+            'use_path_style_endpoint' => env('MINIO_USE_PATH_STYLE_ENDPOINT', env('AWS_USE_PATH_STYLE_ENDPOINT', false)),
             'throw' => false,
         ],
 
